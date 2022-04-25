179ac5bc119e46d2d43b6e43b92fdfedd5dc11be
callumzhong <callum.zhong@gmail.com>
2022-04-25 09:24:31 +0800

    feat: 建立資料庫連線

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const mongoose = require('mongoose');
+const DATABASE = require('./config');
 const postsRoute = require('./routes/posts');
 const PORT = process.env.PORT || 3005;
 const posts = [];
+mongoose
+	.connect(DATABASE)
+	.then(() => {
+		console.log('database: 資料庫連線成功');
+	})
+	.catch((error) => {
+		console.log(`database: 連線失敗 ${error.message}`);
+	});
 const listener = (req, res) => {
 	postsRoute({ req, res, data: posts });
 };
