1916a409b8e9638ac6b7d9f6763178bf654286ad
callumzhong <callum.zhong@gmail.com>
2022-05-14 23:00:11 +0800

    feat: 移除舊程式碼，建立 express 架構

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+require('dotenv').config({
+	path: './config.env',
+});
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const postRouter = require('./routes/post');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/posts', postRouter);
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+module.exports = app;
