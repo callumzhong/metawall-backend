79e4eb1910958ced941040cfbeb32d74541772ce
callumzhong <callum.zhong@gmail.com>
2022-05-25 23:42:29 +0800

    feat: 實作貼文分頁

--- service/postHandler.js
+++ service/postHandler.js
@@ -1,12 +1,38 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports = {
-	getPagination: async () => {},
-	getAll: async () => {},
-	getOne: async () => {},
+	getPagination: async (query) => {
+		const sortParams = query.sort === 'asc' ? 'createdAt' : '-createdAt';
+		const q = query.q.trim ? { content: new RegExp(query.q) } : {};
+		const page = parseInt(query.page);
+		const pageSize = 20;
+
+		const post = await Post.find(q)
+			.populate({
+				path: 'user',
+				select: 'name photo',
+			})
+			.sort(sortParams)
+			.skip((page - 1) * pageSize)
+			.limit(pageSize);
+		const postCount = await Post.find(q).count();
+		const totalPages = Math.ceil(postCount / pageSize);
+
+		return {
+			data: post,
+			paging: {
+				currentPage: page,
+				totalPage: totalPages,
+				hasPre: page > 1 ? true : false,
+				hasNext: page >= totalPages ? false : true,
+			},
+		};
+	},
+	getOne: async (id) => {},
 	created: async (data) => {
-		const createdPost = await Post.create(data);
-		return createdPost;
+		const post = await Post.create(data);
+		return post;
 	},
 	updated: async () => {},
 	deleteAll: async () => {},
